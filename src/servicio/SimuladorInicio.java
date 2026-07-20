ce26e76c38ee3e1fdb0242bef895aed1baa1f9f8
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-20 13:54:53 -0500

    commit - SimuladorInicio / .

--- src/servicio/SimuladorInicio.java
+++ src/servicio/SimuladorInicio.java
@@ -1,4 +1,26 @@
 package servicio;
 
+import dao.DispositivoDAO;
+import dao.EventoDAO;
+import modelo.Dispositivo;
+import modelo.EventoInicio;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ==========================================================
+ * Clase: SimuladorInicio
+ * Proyecto: SimuladorInicioPC
+ * Java 21
+ * ==========================================================
+ *
+ * Simula el proceso de arranque de un computador,
+ * mostrando cada etapa en consola y almacenando
+ * dispositivos y eventos en MySQL .
+ *
+ */
 public class SimuladorInicio {
+
 }
